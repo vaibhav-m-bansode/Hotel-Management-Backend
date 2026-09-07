e2327f2c007ba32519e1f2a23e68eaea26b94138
Vaibhav Bansode <160023851+vaibhav-m-bansode@users.noreply.github.com>
2026-09-07 11:41:50 +0530

    fix: populate API error response

--- src/main/java/com/example/airbnb/Advice/ApiError.java
+++ src/main/java/com/example/airbnb/Advice/ApiError.java
@@ -1,17 +1,18 @@
 package com.example.airbnb.Advice;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
-@Data
 @Getter
 @Setter
 public class ApiError {
+
     private String message;
     private HttpStatus status;
 
-    public ApiError(String localizedMessage, HttpStatus httpStatus) {
+    public ApiError(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status;
     }
 }
